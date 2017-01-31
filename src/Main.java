8f0fbb61ce929776d84f3c5c8c7c04c61cadbe92
Joe Morris <joe.morris.dev@gmail.com>
2017-01-30 19:26:01 -0600

    added key filters. Bugs present

--- src/Main.java
+++ src/Main.java
@@ -17,12 +17,12 @@ public class Main {
         System.out.println("Enter the key you want: ");
         String selectedKey = scanner.nextLine().trim();
         ArrayList<String> transposedScale = IonianScaleTransposition(selectedKey, notes);
-        for(String note : transposedScale){
+        for (String note : transposedScale) {
             System.out.println(note);
         }
     }
 
-    public static String majorMinorSelectionMenu(){
+    public static String majorMinorSelectionMenu() {
         String selection = "";
         ArrayList<String> validInput = new ArrayList<>();
         validInput.add("1");
@@ -43,45 +43,125 @@ public class Main {
             System.out.println("6. Aeolian: Minor");
             System.out.println("7. Locrian: Minor");
             selection = scanner.nextLine();
-        }while (!validInput.contains(selection));
+        } while (!validInput.contains(selection));
         return selection;
     }
 
-    public static ArrayList<String> IonianScaleTransposition(String key, ArrayList<String> notes){
-        ArrayList<String> transposedScale = new ArrayList<>();
-        int root = notes.indexOf(key);
-        transposedScale.add(notes.get(root));
-        transposedScale.add(notes.get((root + 3) % 17));
-        transposedScale.add(notes.get((root + 5) % 17));
-        transposedScale.add(notes.get((root + 7) % 17));
-        transposedScale.add(notes.get((root + 10) % 17));
-        transposedScale.add(notes.get((root + 12) % 17));
-        transposedScale.add(notes.get((root + 14) % 17));
+    public static ArrayList<String> IonianScaleTransposition(String key, ArrayList<String> notes) {
 
-        //similar to caesar cipher
-        //repeats on major 4th
-        // 17 notes
+        loadNotes();
+
+        ArrayList<String> transposedScale = new ArrayList<>();
+        int rootNumber = setRootIndex(key);
+        String rootNote = setRootNote(key);
+
+        transposedScale.add(rootNote);
+        transposedScale.add(notes.get((rootNumber + 2) % 12));
+        transposedScale.add(notes.get((rootNumber + 4) % 12));
+        transposedScale.add(notes.get((rootNumber + 5) % 12));
+        transposedScale.add(notes.get((rootNumber + 7) % 12));
+        transposedScale.add(notes.get((rootNumber + 9) % 12));
+
+        if (key.equals("A")) {
+            transposedScale.add(notes.get((rootNumber + 10) % 12));
+        } else {
+            transposedScale.add(notes.get((rootNumber + 11) % 12));
+        }
+
+        boolean isKeyFlat = isKeyFlat(key);
+        transposedScale = purgeSharpOrFlatNotes(isKeyFlat, transposedScale);
 
         return transposedScale;
     }
 
-    public static void loadNotes(){
-        notes.add("Ab");
+    public static Integer setRootIndex(String key) {
+        int root = 0;
+
+        for (String note : notes) {
+            String[] steps = note.split("/");
+            for (String s : steps) {
+                if (s.equals(key)) {
+                    root = notes.indexOf(note);
+                }
+            }
+        }
+        return root;
+    }
+
+    public static String setRootNote(String key) {
+        String rootNote = "";
+        for (String note : notes) {
+            String[] steps = note.split("/");
+            for (String s : steps) {
+                if (s.equals(key)) {
+                    rootNote = s;
+                }
+            }
+        }
+        return rootNote;
+    }
+
+    public static ArrayList<String> purgeSharpOrFlatNotes(Boolean isFlat, ArrayList<String> transposedScale) {
+        ArrayList<String> revisedScale = new ArrayList<>();
+
+        String note = "";
+
+        for (String n : transposedScale) {
+            boolean firstNote = n.equals(transposedScale.get(0));
+            if (firstNote) {
+                revisedScale.add(n);
+            }
+            else{
+                String[] steps = n.split("/");
+                boolean skip = false;
+                for (String s : steps) {
+
+                    if (n.contains("b") && isFlat && !skip) {
+                        note = steps[1];
+                        skip = true;
+                    }else if(skip){
+                        break;
+                    } else {
+                        note = steps[0];
+                        skip = true;
+                    }
+                    revisedScale.add(note);
+                }
+            }
+        }
+        return revisedScale;
+    }
+
+    public static Boolean isKeyFlat(String key) {
+        boolean isFlat = false;
+
+        if (key.charAt(1) == 'b' || key.length() == 1) {
+            isFlat = true;
+        }
+        return isFlat;
+    }
+
+    public static Boolean isKeySharp(String key) {
+        boolean isSharp = false;
+
+        if (key.charAt(1) == '#' || key.length() == 1) {
+            isSharp = true;
+        }
+        return isSharp;
+    }
+
+    public static void loadNotes() {
         notes.add("A");
-        notes.add("A#");
-        notes.add("Bb");
+        notes.add("A#/Bb");
         notes.add("B");
         notes.add("C");
-        notes.add("C#");
-        notes.add("Db");
+        notes.add("C#/Db");
         notes.add("D");
-        notes.add("D#");
-        notes.add("Eb");
+        notes.add("D#/Eb");
         notes.add("E");
         notes.add("F");
-        notes.add("F#");
-        notes.add("Gb");
+        notes.add("F#/Gb");
         notes.add("G");
-        notes.add("G#");
+        notes.add("G#/Ab");
     }
 }
